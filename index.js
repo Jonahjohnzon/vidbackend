bede1ba2900fdf77c7284fde67e6d849ca009363
loozebox <loozebox@Loozebox.Loozebox>
2023-12-23 10:15:53 +0100

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const https = require('https')
 const cors = require('cors')
 const mongoose = require('./mongo')
+const fs = require('fs')
 const router = require('./Routes/routes')
+const path = require('path')
 require('dotenv').config()
 app.use(cors())
 mongoose()
@@ -11,7 +14,11 @@ const port = process.env.PORT || 5000
 app.use(require('express').json())
 app.use('/', router)
 
+const secure = https.createServer({
+    key: fs.readFileSync('/cert/key.pem'),
+    cert: fs.readFileSync('/cert/cert.pem'),
+}, app)
 
-app.listen(port,()=>{
+secure.listen(port,()=>{
     console.log('Connected to port ' + port)
 })

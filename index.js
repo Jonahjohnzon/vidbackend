79a0e325f299d95ad68eb29d177a78af49c04f2a
loozebox <loozebox@Loozebox.Loozebox>
2023-12-26 18:19:47 +0100

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const app = express()
-const https = require('https')
-const cors = require('cors')
-const mongoose = require('./mongo.js')
-const fs = require('fs')
-const router = require('./Routes/routes.js')
-const path = require('path')
-require('dotenv').config()
-app.use(cors())
-mongoose()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const https = require('https');
+const cors = require('cors');
+const mongoose = require('./mongo.js');
+const fs = require('fs');
+const router = require('./Routes/routes.js');
+const path = require('path');
+require('dotenv').config();
 
-app.use(require('express').json())
-app.use('/', router)
+app.use(cors());
+mongoose();
+
+// Middleware to set X-Forwarded-Host header
+app.use((req, res, next) => {
+    res.setHeader('X-Forwarded-Host', req.hostname);
+    next();
+});
+
+app.use(require('express').json());
+app.use('/', router);
 
 const secure = https.createServer({
     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-}, app)
+}, app);
 
-secure.listen(port,()=>{
-    console.log('Connected to port ' + port)
-})
+const port = process.env.PORT || 5000;
+secure.listen(port, () => {
+    console.log('Connected to port ' + port);
+});

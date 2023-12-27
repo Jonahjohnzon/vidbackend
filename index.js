b97e26e9f3a40deadedc04ef261941589810e7df
loozebox <loozebox@Loozebox.Loozebox>
2023-12-27 10:57:50 +0100

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const app = express();
-const https = require('https');
-const cors = require('cors');
-const mongoose = require('./mongo.js');
-const fs = require('fs');
-const router = require('./Routes/routes.js');
-const path = require('path');
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const https = require('https')
+const cors = require('cors')
+const mongoose = require('./mongo.js')
+const fs = require('fs')
+const router = require('./Routes/routes.js')
+const path = require('path')
+require('dotenv').config()
+app.use(cors())
+mongoose()
+const port = process.env.PORT || 5000
 
-app.use(cors());
-mongoose();
-
-
-app.use(require('express').json());
-app.use('/', router);
+app.use(require('express').json())
+app.use('/', router)
 
 const secure = https.createServer({
     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
 }, app);
 
-const port = process.env.PORT || 5000;
-secure.listen(port, () => {
-    console.log('Connected to port ' + port);
-});
+
+secure.listen(port,()=>{
+    console.log('Connected to port ' + port)
+})

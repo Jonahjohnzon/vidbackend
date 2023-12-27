9c123a5c76f964f33847f763c85e21f95c34db8f
loozebox <loozebox@Loozebox.Loozebox>
2023-12-27 13:30:33 +0100

    first commit

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ app.use((req, res, next) => {
 app.use(require('express').json());
 app.use('/', router);
 
-
+const secure = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+secure.listen(port, () => {
     console.log('Connected to port ' + port);
 });
